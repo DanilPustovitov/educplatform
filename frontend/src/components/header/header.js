65a1d8ac28487dcdb159e8c44bb966a5d3412741
Cardorel <54881922+Cardorel@users.noreply.github.com>
2022-11-11 21:46:28 +0100

    worked on knowledge part

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import './header.scss'
 
@@ -9,18 +8,3 @@ export default function Header() {
       </div>
   )
 }
-=======
-import React from 'react'
-import './header.scss'
-
-export default function Header() {
-  return (
-    <div className='header'>
-       <span className='logo'><p className='logotext'>Логотип</p></span>
-       <span className='searchBtn'> <img src='searchico.png'/></span>
-       <span className='languageBtn'> <img src='languageico.png'/></span>
-       <span className='authBtn'> <img src='joinus.png'/></span>
-    </div>
-  )
-}
->>>>>>> 4ab92f3ecc92e79c9d12e497c38822f526c4d17d
